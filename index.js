21e407e7a3e6cfdcbd81b16e84187129f13ef8d0
Takumi Adachi <takumiadachi@gmail.com>
2019-04-11 17:58:09 -0700

    Added .env. Added .ejs and move upload.thml to .ejs format. Got uploading a file to work.

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
 const app = express();
 app.use(fileUpload());
+app.use(express.json());
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send(200, "<h1>Hello, world!</h1>");
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready On Server http://localhost:${port}`);
 });
 
-app.get("/up", (req, res) => {
-  res.sendFile(path.join(__dirname + "/up.html"));
+app.get("/", (req, res) => {
+  res.render(path.join(__dirname, "views/up"), { URL: process.env.URL });
+});
+
+app.post("/post", function(request, response) {
+  response.send({ yousaid: request.body });
 });
 
 app.post("/upload", function(req, res) {
+  if (req.files === null) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  console.log(req.files);
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send("No files were uploaded.");
+  } else {
+    return res.status(200).send("GOOD!");
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   // let sampleFile = req.files.sampleFile;
-  console.log(req);
+
   // // Use the mv() method to place the file somewhere on your server
   // sampleFile.mv("/somewhere/on/your/server/filename.jpg", function(err) {
   //   if (err) return res.status(500).send(err);
