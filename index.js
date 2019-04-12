847f768e6046bca4c76fc075bdbad72abcfc2313
Takumi Adachi <takumiadachi@gmail.com>
2019-04-12 14:13:11 -0700

    Added file size limits client-side and server-side. Added helmet.js

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, `.env.${process.env.ENVIRONMENT}`) //.env.dev .denv.prod
+});
 const port = process.env.PORT || 3000;
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const path = require("path");
+const helmet = require("helmet");
 
 const app = express();
-app.use(fileUpload());
+const maxSize = 2 * 1024 * 1024;
+
+app.use(
+  fileUpload({
+    limits: { fileSize: maxSize },
+    safeFileNames: true,
+    createParentPath: true,
+    preserveExtension: 3,
+    abortOnLimit: true
+  })
+);
 app.use(express.json());
+app.use(helmet());
 app.set("view engine", "ejs");
 
 app.listen(port, err => {
   if (err) throw err;
   console.log(`> Ready On Server http://localhost:${port}`);
+  console.log(`> Ready On Server http://${process.env.URL}`);
 });
 
 app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "views/up"), { URL: process.env.URL });
-});
-
-app.post("/post", function(request, response) {
-  response.send({ yousaid: request.body });
+  res.render(path.join(__dirname, "views/up"), {
+    URL: process.env.URL,
+    maxSize: maxSize
+  });
 });
 
 app.post("/upload", function(req, res) {
   if (req.files === null) {
     return res.status(400).send("No files were uploaded.");
   }
-  console.log(req.files);
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send("No files were uploaded.");
-  } else {
-    return res.status(200).send("GOOD!");
   }
-
+  console.log(req.files);
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  // let sampleFile = req.files.sampleFile;
+  let fileToUpload = req.files.fileToUpload;
 
-  // // Use the mv() method to place the file somewhere on your server
-  // sampleFile.mv("/somewhere/on/your/server/filename.jpg", function(err) {
-  //   if (err) return res.status(500).send(err);
+  // Use the mv() method to place the file somewhere on your server
+  const f = path.join(__dirname, `/files/${fileToUpload.name}`);
+  fileToUpload.mv(f, function(err) {
+    if (err) return res.status(500).send(err);
 
-  //   res.send("File uploaded!");
-  // });
+    res.send("File uploaded!");
+  });
 });
 
 module.exports = app;
